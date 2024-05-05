70a4a9c7053acc0d3c67f3c9b752e54ccfca6be5
Richard Tweed <RichardoC@users.noreply.github.com>
2024-05-05 19:42:51 +0100

    Make it work with streaming

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"io"
 	"log/slog"
 	"net/http"
 	"os"
@@ -31,6 +33,19 @@ func scrapeSite(url string) (string, error) {
 		return "", err
 	}
 
+	// doc.Find("img").Remove()
+
+	doc.Find("img").Each(func(i int, el *goquery.Selection) {
+		slog.Error(fmt.Sprintf("Found img %+v", el))
+		fmt.Println()
+		fmt.Println()
+		fmt.Println()
+		el.Remove()
+	})
+	// We don't care about images, we only want the text on the site
+	doc.Find("picture").Each(func(i int, el *goquery.Selection) {
+		el.Remove()
+	})
 	// We don't care about scripts, we only want the text on the site
 	doc.Find("script").Each(func(i int, el *goquery.Selection) {
 		el.Remove()
@@ -39,18 +54,31 @@ func scrapeSite(url string) (string, error) {
 	doc.Find("style").Each(func(i int, el *goquery.Selection) {
 		el.Remove()
 	})
+	// We don't care about videos, we only want the text on the site
+	doc.Find("video").Each(func(i int, el *goquery.Selection) {
+		el.Remove()
+	})
 
 	// remove excess newlines etc
 	bdyText := standardizeSpaces(doc.Text())
+	slog.Info(bdyText)
 	return bdyText, err
 }
 
-func doSummarisation(token string, server string, model string, siteText string) (summary string, err error){
+func doSummarisation(token string, maxTokens int, server string, model string, siteText string) (summary string, err error) {
+
+	lengthOfDataToSend := len(siteText)
+
+	// Just a rule of thumb
+	if len(siteText) > maxTokens {
+		lengthOfDataToSend = 2 * maxTokens
+	}
+
 	aiClient := openai.DefaultConfig(token)
 	aiClient.BaseURL = server
 	client := openai.NewClientWithConfig(aiClient)
 
-	resp, err := client.CreateChatCompletion(
+	stream, err := client.CreateChatCompletionStream(
 		context.Background(),
 		openai.ChatCompletionRequest{
 			Model: model,
@@ -59,32 +87,62 @@ func doSummarisation(token string, server string, model string, siteText string)
 					Content: "You are an expert at summarising text in a concise manner for intelligent social media users.",
 				},
 				{
-					Role:    openai.ChatMessageRoleUser,
-					Content: fmt.Sprintf("Summarise the following text\n, %s", siteText),
+					Role: openai.ChatMessageRoleUser,
+					Content: fmt.Sprintf("Summarise the following text using the fewest possible words\n, %s",
+						// siteText),
+						siteText[:lengthOfDataToSend]),
 				},
 			},
+			Stream:           true,
 			Stop:             []string{"<|end_of_text|>", "<|eot_id|>"},
 			Temperature:      0.7,
 			TopP:             0.95,
-			MaxTokens:        4096,
+			MaxTokens:        maxTokens,
 			FrequencyPenalty: 0.0,
 			PresencePenalty:  0.0,
 		},
 	)
+
 	if err != nil {
-		slog.Error("Error with summarisation", "error", err)
+		slog.Error("Error with ChatCompletionStream for summariser", "error", err)
 		return "", err
 	}
+	defer stream.Close()
 
-	return resp.Choices[0].Message.Content, nil
+	var summaryBuilder strings.Builder
+	for {
+		var response openai.ChatCompletionStreamResponse
+		response, err = stream.Recv()
+		if errors.Is(err, io.EOF) {
+			slog.Debug("Finished receiving response from LLM")
+			return summaryBuilder.String(), nil
+		}
+
+		if err != nil {
+			slog.Error("Error with ChatCompletionStream for summariser", "error", err)
+			return summaryBuilder.String(), err
+		}
+		_, err = summaryBuilder.WriteString(response.Choices[0].Delta.Content)
+
+		if err != nil {
+			slog.Error("Error with writing stream to summary", "error", err)
+			return summaryBuilder.String(), err
+		}
+
+	}
 
 }
 
 func main() {
-	site := "http://example.com/"
+
+	// Replace this with a function to discover
+	// site := "https://www.bbc.co.uk/news/uk-england-london-68552817"
+	site := "https://github.com/PuerkitoBio/goquery"
+	// site := "https://example.com"
 
 	siteText, err := scrapeSite(site)
 
+	// error already logged
 	if err != nil {
 		os.Exit(4)
 	}
@@ -94,6 +152,19 @@ func main() {
 	token := "fake-token"
 	server := "http://127.0.0.1:1337/v1"
 	model := "llama3-8b-instruct"
+	maxTokens := 4096
 
-	fmt.Println(doSummarisation(token, server, model, siteText))
+	summary, err := doSummarisation(token, maxTokens, server, model, siteText)
+	// error already logged
+	if err != nil {
+		os.Exit(4)
+	}
+
+	slog.Debug("summary text obtained", "text", summary)
+
+	fmt.Println(summary)
+	reduction := float64(len(siteText) - len(summary))
+	lengthRatio := (reduction) / float64(len(siteText))
+
+	fmt.Printf("The original site contains %d words, the summary contains %d words. Saved %f %%\n", len(siteText), len(summary), lengthRatio*100)
 }
